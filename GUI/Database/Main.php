cd36ea653bb22e33880a9968b3a59b50818df8ea
Lucksi <79308097+Lucksi@users.noreply.github.com>
2021-09-02 21:06:14 +0200

    Add files via upload

--- GUI/Database/Main.php
+++ GUI/Database/Main.php
@@ -7,22 +7,22 @@ License: GNU General Public License v3.0-->
         <title>Dashboard</title>
         <?php
             $mode_file = "../Theme/Mode.json";
-            if (file_exists($mode_file)) {
-            $reader = file_get_contents($mode_file);
-            $parser = json_decode($reader,true); 
-            $color = $parser["Color"]["Background"];
-            $Style_name = "../Css/$color/Main.css";
-            if (file_exists($Style_name)) {
-                echo '<link rel = "stylesheet" href ="'.$Style_name.'">';                
-            }
-            else {
-                echo "<script>
-                alert ('VALUE NOT VALID MODE DISPLAYED:LIGHT-MODE');
-                </script>";
-                echo '<link rel = "stylesheet" href = "../Css/Light/Main.css">';
-                    }
+            if (file_exists($mode_file)){
+                $reader = file_get_contents($mode_file);
+                $parser = json_decode($reader,true); 
+                $color = $parser["Color"]["Background"];
+                $Style_name = "../Css/$color/Main.css";
+                if (file_exists($Style_name)){
+                    echo '<link rel = "stylesheet" href ="'.$Style_name.'">';                
                 }
-            else {
+                else{
+                    echo "<script>
+                    alert ('VALUE NOT VALID MODE DISPLAYED:LIGHT-MODE');
+                    </script>";
+                    echo '<link rel = "stylesheet" href = "../Css/Light/Main.css">';
+                }
+            }
+            else{
                 echo "<script>
                 alert ('THEME FILE NOT FOUND MODE DISPLAYED:LIGHT-MODE');
                 </script>";
@@ -34,6 +34,7 @@ License: GNU General Public License v3.0-->
         <meta charset ="UTF-8">
         <script src = "../../Script/Language.js"></script>
         <script src = "../../Script/Author.js"></script>
+        <script src = "../../Script/Arrow.js"></script>
     </head>
     <body>
         <div class = "Top-bar">
@@ -51,19 +52,30 @@ License: GNU General Public License v3.0-->
             <p id = "Const">A COMPLETE OSINT TOOL</p>
             <div class = "Cards">
                 <div id = "Username">
+                    <img src = "../Icon/social.png">
                     <p id = "Const">USERNAME</p>
                     <a href = "Username.php"><button>Search</button></a>
                 </div>
                 <div id = "Website">
+                    <img src = "../Icon/browser.png">
                     <p id = "Const">WEBSITE</p>
                     <a href = "Websites.php"><button>Search</button></a>
                 </div>
                 <div id = "Phone">
+                    <img src = "../Icon/phone.png">
                     <p id = "Const">Phone</p>
                     <a href = "Phone.php"><button>Search</button></a>
                 </div>
             </div>
         </div>
+        <a href = "#Footer" id = "Arrow" onclick="Active()"></a>
+        <div class = "Footer" id = "Footer" name = "Footer">
+            <p id = "Const">MY-CONTACTS</p>
+            <div class = "Board">
+                <a href = "https://instagram.com/lucks_022"><img src = "../Icon/instagram.png" id = "Exc"></a>
+                <a href = "mailto:lukege287@gmail.com"><img src = "../Icon/Email.png"></a>
+                <a href = "https://github.com/Lucksi"><img src = "../Icon/Git-hub.png"></a>
+            </div>
+        </div>
     </body>
-
 </html>
