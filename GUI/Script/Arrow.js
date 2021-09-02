d39f2bfca96f8879d03369b482713f19bf6fdfb5
Lucksi <79308097+Lucksi@users.noreply.github.com>
2021-09-02 21:11:02 +0200

    Update Arrow.js

--- GUI/Script/Arrow.js
+++ GUI/Script/Arrow.js
@@ -4,11 +4,10 @@ License: GNU General Public License v3.0*/
 
 function Active(){
     document.getElementById("Footer").style.display= "block";
-    document.getElementById("Arrow").setAttribute("onClick", "javascript: Deactive();" )
+    document.getElementById("Arrow").setAttribute("onClick", "javascript: Deactive();" );
 } 
 
 function Deactive(){
     document.getElementById("Footer").style.display= "none";
-    document.body.style.animation= "dissolvent 2s 1";
-    document.getElementById("Arrow").setAttribute("onClick", "javascript: Active();" )
+    document.getElementById("Arrow").setAttribute("onClick", "javascript: Active();" );
 }
