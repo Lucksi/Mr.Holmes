7fce16c3e06e9209fc40de9565de53bfa2f2684e
Lucksi <79308097+Lucksi@users.noreply.github.com>
2021-10-03 16:22:30 +0200

    Add New Gui Folder

---
+++ GUI/Actions/Session_Checker.php
@@ -0,0 +1,29 @@
+<!--AUTHOR: Lucksi
+Copyright © 2021 Lucksi
+License: GNU General Public License v3.0--> 
+<?php
+    function Moderate(){
+        $Session_Token = "../Session/Token.txt";
+        $Login_file = "../Credentials/Login.json";
+        if(file_exists($Login_file)){    
+            $reader = file_get_contents($Login_file);
+            $Parser = json_decode($reader,true);
+            $Status = $Parser["Database"]["Status"];
+            if($Status == "Active"){   
+                if(file_exists($Session_Token)){
+                    header("Location: ");
+                }
+                else {
+                    header("Location: ../Login/Login.php");
+                }
+            }
+            else {
+                header("Location: ");
+            }
+        }
+        else {
+            die;
+        }
+    }
+    Moderate();
+?>
---
+++ GUI/index.php
@@ -0,0 +1,6 @@
+<!--AUTHOR: Lucksi
+Copyright © 2021 Lucksi
+License: GNU General Public License v3.0-->
+<?php
+require("Actions/Login_Controller.php");
+?>
