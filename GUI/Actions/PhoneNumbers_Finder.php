f815b6795f713ab652f83e5faa53236ccf800157
Lucksi <79308097+Lucksi@users.noreply.github.com>
2021-10-25 15:28:59 +0200

    Update GUI

--- GUI/Actions/PhoneNumbers_Finder.php
+++ GUI/Actions/PhoneNumbers_Finder.php
@@ -18,7 +18,7 @@ License: GNU General Public License v3.0-->
                 alert('NUMBER FOUND');
                 </script>";
                 echo "<p id = 'Const'>NUMBER DATA</p>";
-                echo "<div class = 'Data'>";
+                echo "<div class = 'Datap'>";
                 echo "<p id = 'Const'>REPORT:</p>";
                 $data = fopen($Complete_name,"r")or die("Sever-Error");
                 while (!feof($data)){
@@ -27,7 +27,20 @@ License: GNU General Public License v3.0-->
                 }
                 fclose($data);
                 echo "</p>";
-                echo "\n</div>";     
+                echo "\n</div>";
+                $Complete_name = "../Reports/Phone/Dorks/{$File_name}_dorks.txt";
+                if(file_exists($Complete_name)){
+                    echo "<div class = 'Dataf'>";
+                    echo "<p id = 'Const'>DORKS:</p>";
+                    $data = fopen($Complete_name,"r")or die("Sever-Error");
+                    while (!feof($data)){
+                        $content = fgets($data);
+                        echo "<p>".$content;
+                    }
+                    fclose($data);
+                    echo "</p>";
+                    echo "\n</div>";     
+                }
             }
             else {
                 echo "
--- GUI/Actions/Usernames_Finder.php
+++ GUI/Actions/Usernames_Finder.php
@@ -29,6 +29,22 @@ License: GNU General Public License v3.0-->
                     fclose($data);
                     echo "</p>";
                     echo "\n\t\t</div>";
+                    $Complete_name = "../Reports/Usernames/Dorks/{$File_name}_Dorks.txt";
+                    if(file_exists($Complete_name)){
+                        echo "\n\t\t<div class = 'Data2'>";
+                        $data = fopen($Complete_name,"r")or die("Server-Error");
+                        echo "\n\t\t\t<p id = Const>DORKS-LIST:</p>\n";
+                        while (!feof($data)){
+                            $content = fgets($data);
+                            echo "\t\t\t<p>".$content."</p>";
+                        }
+                        fclose($data);
+                        echo "</p>";
+                        echo "\n\t\t</div>";
+                    }
+                    else{
+
+                    }
                     echo "\n\t\t<div class = 'Data_img'>";
                     $Dir_Name = "../Reports/Usernames/Profile_pics/{$File_name}/";
                     if(file_exists($Dir_Name)){
@@ -43,6 +59,7 @@ License: GNU General Public License v3.0-->
                         echo "\n\t\t\t<p id = 'error'>NOT FIND ANY PROFILE PIC FOR THIS USER</p>";
                     }
                     echo "\n\t\t</div>";
+                    echo "</div>";
                 }
                 else {
                     echo "
