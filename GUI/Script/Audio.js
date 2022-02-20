bfc9107902c1e934877f0f320dbcaf58e072fec6
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2022-02-20 20:52:23 +0100

    Delete Old GUI Directory

--- GUI/Script/Audio.js
+++
@@ -1,8 +0,0 @@
-/*AUTHOR: Luca Garofalo (Lucksi)
-Copyright (C) 2021-2022 Lucksi
-License: GNU General Public License v3.0*/
-
-function Play() {
-    var effect = new Audio ("../Sounds/Uchiha/Sound.mp3");
-    effect.play();
-}
