f97d98841a5767661d95e86a72ce497c3c5afdd2
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2022-04-04 22:46:55 +0200

    Update GUI

--- GUI/Actions/PhoneNumbers_Finder.php
+++ GUI/Actions/PhoneNumbers_Finder.php
@@ -21,6 +21,29 @@
         }
     }
 
+    function Maps_Generator($File_name){
+        echo "<br>";
+        echo "<div class = 'Geo'>";
+        echo "<p id = 'Const'>PHONE-GEOLOCATION</p>";
+        $Ip_File = "../Reports/Phone/{$File_name}/GeoLocation.json";
+        $reader = file_get_contents($Ip_File);
+        $parser = json_decode($reader,true);
+        $Latitude = $parser["Geolocation"]["Latitude"];
+        $Longitude = $parser["Geolocation"]["Longitude"];
+        echo "
+        <div class = 'map' id='map'></div>
+        <script>
+        var map = L.map('map').setView([$Latitude,$Longitude], 14);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href= https://www.openstreetmap.org/copyright >OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([$Latitude,$Longitude]).addTo(map)
+        .bindPopup('Your Target is approximatley based in this Area.')
+        .openPopup();
+        </script>";
+    }
+
     function Get_List($File_name,$Complete_name){
         echo "<div class = 'Wrapper2'>";
         echo "\n\t\t<div class = 'Data_img3'>";
@@ -77,6 +100,8 @@
                 fclose($data);
                 echo "</p>";
                 echo "\n</div>";
+                Maps_Generator($File_name);
+                echo "</div>";
                 $Complete_name = "../Reports/Phone/Dorks/{$File_name}_dorks.txt";
                 get_dorks($Complete_name);
                 $Complete_name = "../Reports/Phone/{$File_name}/{$File_name}.txt";
