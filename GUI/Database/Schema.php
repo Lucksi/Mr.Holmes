047ed0b00a30e84c6288e21e2231cbb1db9ed493
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2022-08-22 09:21:36 +0200

    Add files via upload

--- GUI/Database/Schema.php
+++ GUI/Database/Schema.php
@@ -1,3 +1,7 @@
+<!--ORIGINAL CREATOR: Luca Garofalo (Lucksi)
+AUTHOR: Luca Garofalo (Lucksi)
+Copyright (C) 2022 Lucksi <lukege287@gmail.com>
+License: GNU General Public License v3.0-->
 <!DOCTYPE HTML>
 <html>
     <head>
