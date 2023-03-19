b35009e43580db4d50341bb4ef626e373bb9bb94
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2023-03-19 10:39:55 +0100

    Modified File-Transfer Css

--- Transfer/index.php
+++ Transfer/index.php
@@ -13,48 +13,6 @@ License: GNU General Public License v3.0-->
                     echo "<a href = '$reader' download><button id = 'clicker'>Download</button></a>";
                 }
             ?>
-            <style>
-                .Bolder{
-                    font-size: 15px;
-                    font-weight:bold;
-                    color: black;
-                    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-                    margin-top:30px
-                }
-                #Main {
-                    margin-top:none;
-                    font-weight:bold;
-                    color: black;
-                    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-                    font-size:23px;
-                    margin-top:-5px;
-                    margin-left:-5px;
-                }
-                button{
-                    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-                    font-size:18px;
-                    background-color:blue;
-                    color:white;
-                    border:3px solid black;
-                    border-radius:10px;
-                }
-                button:hover{
-                    animation:click 2s infinite;
-                }
-                .Content{
-                    margin-top:100px;
-                }
-                img{
-                    margin-top:-50px; 
-                    height:80px;
-                    width:80px;
-                }
-                @keyframes click {
-                    0%{background-color:blue;color:white;}
-                    50%{background-color:rgb(0, 255, 98);color:white;}
-                    100%{background-color:blue;color:white;}
-                }
-            </style>
             <script>
                 function download(){
                     var text = document.getElementById("Download");
@@ -68,6 +26,53 @@ License: GNU General Public License v3.0-->
                     const myTimeout = setTimeout(download, 5000);
                 }
             </script>
+            <style>
+                body{
+                    background-color: #1a2b34;                
+                }
+                .Bolder{
+                    font-size: 20px;
+                    font-weight:bold;
+                    color: white;
+                    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                    margin-top:30px
+                }
+                #Main {
+                    margin-top:none;
+                    font-weight:bold;
+                    color: white;
+                    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                    font-size:23px;
+                    margin-top:-5px;
+                    margin-left:-5px;
+                }
+                button{
+                    border: 3px solid black;
+                    background-color: #ff4500;
+                    height: 40px;
+                    width: fit-content;
+                    font-family: 'Courier New', Courier, monospace;
+                    font-size: 20px;
+                    font-weight: bold;
+                    color: #f0f8ff;
+                }
+                button:hover{
+                    animation:click 2s infinite;
+                }
+                .Content{
+                    margin-top:100px;
+                }
+                img{
+                    margin-top:-50px; 
+                    height:80px;
+                    width:80px;
+                }
+                @keyframes click {
+                    0%{color: #f0f8ff; background-color: #ff4500;}
+                    50%{color: #000000; background-color: #f0f8ff;}
+                    100%{color: #f0f8ff; background-color: #ff4500;}
+                }
+            </style>
             <meta charset ="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=0.9">
             <meta name="theme-color" content="#000000">
