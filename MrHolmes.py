877bed88d0e397218046c31fc98a34c2ea2fb756
Tony Alex M <ta2650066@gmail.com>
2026-03-09 14:18:58 +0530

    Update MrHolmes.py

--- MrHolmes.py
+++ MrHolmes.py
@@ -1,48 +1,90 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # ORIGINAL CREATOR: Luca Garofalo (Lucksi)
 # AUTHOR: Luca Garofalo (Lucksi)
-# Copyright (C) 2021-2023 Lucksi <lukege287@gmail.com>
 # License: GNU General Public License v3.0
 
 import os
+import sys
+
 from Core.Support import Menu
 from Core.Support import Font
 from Core.Support import Language
 
-filename = Language.Translation.Get_Language()
-filename
+
+# Load language configuration safely
+try:
+    filename = Language.Translation.Get_Language()
+except Exception as e:
+    print("Language configuration error:", e)
+    sys.exit(1)
+
 
 class Main:
 
     @staticmethod
     def Controll_Display():
-        Interface_file = "Display/Display.txt"
-        if os.path.isfile(Interface_file):
-            d = open(Interface_file,"r",newline=None)
-            conf = d.read().strip("\n")
-            d.close()
-            if conf == "Desktop":
-                pass
-            elif conf == "Mobile":
-                pass
-            else:
-                print(Font.Color.RED + "[!]" + Font.Color.WHITE +  Language.Translation.Translate_Language(filename, "Default", "DisplayError", "None"))
-                exit()
-        else:
-             print(Font.Color.RED + "[!]" + Font.Color.WHITE + Language.Translation.Translate_Language(filename, "Default", "NoDisplay", "None") .format(Interface_file))
+        interface_file = "Display/Display.txt"
+
+        # check if file exists
+        if not os.path.isfile(interface_file):
+            print(
+                Font.Color.RED + "[!]" +
+                Font.Color.WHITE +
+                Language.Translation.Translate_Language(
+                    filename, "Default", "NoDisplay", "None"
+                ).format(interface_file)
+            )
+            sys.exit(1)
+
+        # read file
+        with open(interface_file, "r") as d:
+            conf = d.read().strip()
+
+        # validate mode
+        if conf not in ["Desktop", "Mobile"]:
+            print(
+                Font.Color.RED + "[!]" +
+                Font.Color.WHITE +
+                Language.Translation.Translate_Language(
+                    filename, "Default", "DisplayError", "None"
+                )
+            )
+            sys.exit(1)
+
         return conf
 
-    def Menu(Mode):
-        Menu.Main.main(Mode)
+    @staticmethod
+    def Menu(mode):
+        Menu.Main.main(mode)
+
 
 if __name__ == "__main__":
-    Mode = Main.Controll_Display()
-    Mode
+
+    mode = Main.Controll_Display()
+
     try:
-       Main.Menu(Mode)
+        Main.Menu(mode)
+
     except KeyboardInterrupt:
-        print(Font.Color.RED + "\n\n[!]" + Font.Color.WHITE + Language.Translation.Translate_Language(filename, "Default", "KeyC", "None"))
-        exit()
-    except ModuleNotFoundError as Error:
-        print(Font.Color.RED + "\n\n[!]" + Font.Color.WHITE + Language.Translation.Translate_Language(filename, "Default", "Internal", "None").format(str(Error)))
-        exit()
+        print(
+            Font.Color.RED + "\n\n[!]" +
+            Font.Color.WHITE +
+            Language.Translation.Translate_Language(
+                filename, "Default", "KeyC", "None"
+            )
+        )
+        sys.exit(0)
+
+    except ModuleNotFoundError as error:
+        print(
+            Font.Color.RED + "\n\n[!]" +
+            Font.Color.WHITE +
+            Language.Translation.Translate_Language(
+                filename, "Default", "Internal", "None"
+            ).format(str(error))
+        )
+        sys.exit(1)
+
+    except Exception as error:
+        print(Font.Color.RED + "\nUnexpected error:", error)
+        sys.exit(1)
